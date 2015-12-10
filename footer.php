1215fb9a57fbd1d08a4e8f28db6437cdedb169f0
DominicMC <201356743@P104-05.clg.priv>
2015-12-10 13:10:35 -0500

    voila header pi footer

--- footer.php
+++ footer.php
@@ -6,8 +6,10 @@
  * Time: 18:33
  */
 ?>
+<div class="col-lg-12 col-xs-12">
 <footer class="footer">
     <div class="container">
         <p class="text-muted">Dominic Clement & David Sylvestre</p>
     </div>
 </footer>
+</div>
--- gestimage.php
+++ gestimage.php
@@ -41,43 +41,7 @@ if ($uneImage == false)
     die("L'image n'existe pas");
 }
 ?>
-<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
-    <div class="container">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="index.php">Home</a>
-        </div>
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav">
-                <li>
-                    <?php
-                    echo '<a href="profil.php">Profil</a>';
-                    ?>
-                </li>
-                <?php
-                if ($_SESSION["isAdmin"])
-                {
-
-                    echo '<li><a href="gestionUsagers.php">Gestion des usagers</a></li>';
-                }
-                ?>
-                <li>
-                    <?php
-                    echo '<a href="Deconnection.php">Deconnection</a>';
-                    ?>
-                </li>
-
-            </ul>
-        </div><!-- /.navbar-collapse -->
-    </div><!-- /.container-fluid -->
-</nav>
+<?php include 'Header.php' ?>
 <div class="container" style="margin-top: 70px">
     <div class="row">
 
